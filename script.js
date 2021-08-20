45c1a8804122fa52cb33e562ab022c33ca0fa936
maratreason <maratreason@mail.ru>
2021-08-20 11:40:41 +0300

    add tabs

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const buttons = document.querySelector(".btn");
 const sitebarItems = document.querySelector(".sitebar-items");
-
-// ----- sitebar
 const sitebar = document.querySelector(".sitebar");
 let offsetX;
 let offsetY;
+let rowIndex = 0;
 
 sitebar.addEventListener("dragstart", function(event) {
     offsetX = event.offsetX;
@@ -27,6 +25,8 @@ sitebar.addEventListener("dragend", function(event) {
 
 const site = document.querySelector(".site");
 let current;
+let rows;
+let cols;
 
 sitebarItems.addEventListener("dragstart", function(event) {
     current = event.target;
@@ -36,9 +36,105 @@ site.addEventListener("dragover", function(event) {
     event.preventDefault();
 });
 
-site.addEventListener("drop", function(event) {
-    let nodeCopy = current.cloneNode(true);
-    this.appendChild(nodeCopy);
-    delete nodeCopy;
-    nodeCopy.removeAttribute("draggable");
+site.addEventListener("drop", function() {
+    if (current && current.classList.contains("row")) {
+        let nodeCopy = current.cloneNode(true);
+        nodeCopy.setAttribute("data-row", rowIndex)
+        this.appendChild(nodeCopy);
+        rowIndex++;
+    }
+    rows = site.querySelectorAll(".row");
+
+    if (rows) {
+        rows.forEach(row => {
+            row.addEventListener("dragstart", function(event) {
+                current = event.target;
+            });
+        
+            row.addEventListener("dragover", function(event) {
+                event.preventDefault();
+                event.target.style.borderColor = "#c28b38";
+                row.style.paddingBottom = "50px";
+            });
+
+            row.addEventListener("dragleave", function(event) {
+                event.target.style.borderColor = "#eee";
+                row.style.paddingBottom = "15px";
+            });
+
+            row.addEventListener("dragend", function(event) {
+                event.target.style.borderColor = "#eee";
+                row.style.paddingBottom = "15px";
+            });
+            
+            row.addEventListener("drop", function(event) {
+                event.target.style.borderColor = "#eee";
+                row.style.paddingBottom = "15px";
+                if (row.dataset.row == event.target.dataset.row) {
+                    if (current && current.classList.contains("col")) {
+                        let nodeCopy = current.cloneNode(true);
+                        this.appendChild(nodeCopy);
+                    }
+                    current = null;
+                }
+            });
+
+            cols = row.querySelectorAll(".col");
+
+            if (cols) {
+                cols.forEach(column => {
+                    column.addEventListener("dragstart", function(event) {
+                        current = event.target;
+                    });
+                
+                    column.addEventListener("dragover", function(event) {
+                        event.preventDefault();
+                        event.target.style.borderColor = "#000";
+                        column.style.paddingBottom = "40px";
+                    });
+        
+                    column.addEventListener("dragleave", function(event) {
+                        event.target.style.borderColor = "#ccc";
+                        column.style.paddingBottom = "5px";
+                    });
+        
+                    column.addEventListener("dragend", function(event) {
+                        event.target.style.borderColor = "#ccc";
+                        column.style.paddingBottom = "5px";
+                    });
+                    
+                    column.addEventListener("drop", function(event) {
+                        event.target.style.borderColor = "#ccc";
+                        column.style.paddingBottom = "5px";
+                        if (current) {
+                            let nodeCopy = current.cloneNode(true);
+                            this.appendChild(nodeCopy);
+                        }
+                        current = null;
+                    });
+                });
+            }
+
+        });
+    }
+});
+
+const tabsHeader = document.querySelector(".sitebar-tabs");
+const tabsContent = document.querySelectorAll(".sitebar-tab-content");
+
+tabsHeader.addEventListener("click", function(event) {
+    tabsHeader.querySelectorAll(".tab-header").forEach(el => {
+        el.classList.remove("active")
+    });
+    // let $target = event.target;
+    event.target.classList.add("active");
+
+    tabsContent.forEach(content => {
+        content.classList.remove("active");
+        if (event.target.dataset.id == content.dataset.id) {
+            content.classList.add("active");
+        } else {
+            content.classList.remove("active");
+        }
+    });
 });
